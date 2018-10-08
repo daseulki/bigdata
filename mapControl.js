c9b2d3f55bbc5ee38ae71d5de1ab678fde6fcfee
daseulki <sisial1002@gmail.com>
2018-10-08 17:51:38 +0900

    need to fix scope

--- mapControl.js
+++ mapControl.js
@@ -1,6 +1,8 @@
 ///////////////////////////////////MAP TEST (only frontend)
 ///////////////////////////////////////////////////////////
 
+// Array.from = function (s, t) { return Array.prototype.map.call(s, t || function (i) { return i; }); };
+
 
 /////////////////////////////////////////Declare Variable
 //////////////////////////////////////////////////////////
@@ -19,6 +21,8 @@ let myradius = 1000; //초기값
 let num = 0;
 let groupNum = 0;
 let add = "";
+
+let textFile = null;
 
 /////////////////////////////////////////////////화면 그리기
 ///////////////////////////////////////////////////////////
@@ -48,7 +52,7 @@ vworld.init(
 
 );
 
-function setModeCallback() {
+let setModeCallback = function() {
   vworld.setModeCallback(modecallback);
 }
 
@@ -56,50 +60,50 @@ function setModeCallback() {
 //   map.vworldHybrid.setVisibility(!map.vworldHybrid.getVisibility());
 // }
 
-function addThemeLayer(title, layer) {
+let addThemeLayer = function(title, layer) {
   map.showThemeLayer(title, {
     layers: layer
   });
 }
 
-// function addTileCache() {
-//   map.showTileCacheLayer('산사태위험지도', 'SANSATAI', {
-//     min: 9,
-//     max: 15
-//   });
-// }
+function addTileCache() {
+  map.showTileCacheLayer('산사태위험지도', 'SANSATAI', {
+    min: 9,
+    max: 15
+  });
+}
 
-function setTestProxy() {
+let setTestProxy =  function() {
   alert(OpenLayers.ProxyHost);
 }
 
 ////////////////////////////////////////////css 조절
 ////////////////////////////////////////////////////
 
-function slideLeft() {
+let slideLeft = function() {
   let btn = document.getElementById('mapCheckbox');
   btn.classList.toggle("slideleft");
 
 }
 
-function slideUp() {
+let slideUp = function() {
   let btn = document.getElementById('markerMaker');
   btn.classList.toggle("slideup");
   // console.log('upup')
 }
 
-function openPopup() {
+let openPopup = function() {
   modal.style.display = "block";
 }
 
-function closePopup() {
+let closePopup = function() {
   modal.style.display = "none";
 }
 
 ////////////////////////////////////////////////////////////마커 이벤트
 /////////////////////////////////////////////////////////////////////
 
-function addMarkingEvent() {
+let addMarkingEvent = function() {
   let pointOptions = {
     persist: true
   }; //포인트옵션
@@ -120,7 +124,7 @@ function addMarkingEvent() {
 }
 
 
-function mClick(event) {
+let mClick = function(event) {
   map.init(); //나의 맵 초기화
 
   let checkedOption = $("#mGroup option").index($("#mGroup option:selected"));
@@ -137,7 +141,7 @@ function mClick(event) {
       groupMarker = new vworld.GroupMarker('lora');
       groupMarker = new vworld.GroupMarker('wifi');
       groupMarker = new vworld.GroupMarker('ble');
-      console.log('added groupMarker' + groupMarker);
+      // console.log(`added groupMarker: ${groupMarker}`);
     }
     geocoder_reverse(pos.lon, pos.lat);
   }
@@ -146,13 +150,13 @@ function mClick(event) {
 ///////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////
 
-function setRadius() {
+let setRadius = function() {
   let rad = document.querySelector('input[name="rad"]:checked').value;
   if (rad != "") myradius = rad;
   // console.log("myradius is chaged to" + rad);
 }
 
-function colorChange(group) {
+let colorChange = function(group) {
   let style_orange = {
     strokeColor: "#ff6300",
     strokeOpacity: 0.6,
@@ -185,7 +189,7 @@ function colorChange(group) {
   }
 }
 
-function addMarker(group, lon, lat, message, imgurl) {
+let addMarker = function(group, lon, lat, message, imgurl) {
   let marker = groupMarker.addMarker(group, lon, lat, message, "");
 
   // 마커 아이콘 이미지 파일명 설정합니다.
@@ -222,7 +226,7 @@ function addMarker(group, lon, lat, message, imgurl) {
 
 }
 
-function addOption(optionText) {
+let addOption = function(optionText) {
   let markList = document.getElementById("markerList");
   let markOption = document.createElement("option");
 
@@ -233,7 +237,7 @@ function addOption(optionText) {
   num += 1;
 }
 
-function confirmGroup() {
+let confirmGroup = function() {
 
   let mGroup = document.getElementById("mGroup");
   let mMGroup = document.getElementById("mMGroup");
@@ -261,7 +265,7 @@ function confirmGroup() {
 ///////////////////////////////////////////////////////마커 수정..
 //////////////////////////////////////////////////////////////////
 
-function addGroup() {
+let addGroup = function() {
 
   let mMGroup = document.getElementById("mMGroup");
   let markOption = document.createElement("option");
@@ -271,17 +275,17 @@ function addGroup() {
   markOption.value = groupNum;
   mMGroup.add(markOption, mMGroup[groupNum]);
   groupNum += 1;
-  console.log("group id: " + groupNum)
+  console.log("group id: " + groupNum);
 }
 
-function delGroup(){
+let delGroup = function(){
   let checkedOption = $("#mMGroup option").index($("#mMGroup option:selected"));
   mMGroup.options[checkedOption] = null;
   mGroup.options[checkedOption] = null;
 
 }
 
-function hideGroup() {
+let hideGroup = function() {
   let groupName = document.getElementById('mMarkerGroup').value;
   groupMarker.hideGroup(groupName);
   for (let i = 0; i < tempScope.length; i++) {
@@ -292,7 +296,7 @@ function hideGroup() {
   }
 }
 
-function showGroup() {
+let showGroup = function() {
   let groupName = document.getElementById('mMarkerGroup').value;
   groupMarker.showGroup(groupName);
   for (let i = 0; i < tempScope.length; i++) {
@@ -303,10 +307,7 @@ function showGroup() {
   }
 }
 
-
-
-
-function removeGroup() {
+let removeGroup = function() {
   let groupName = document.getElementById('mMarkerGroup').value;
   let marker = map.userMarkers.markers;
 
@@ -317,11 +318,31 @@ function removeGroup() {
   // }
   //
 
+  //////////////////////////// ES6.. 익스에서 안되네..
+  // for (const curScope of tempScope){
+  //   if(curScope.groupName === groupName){
+  //     map.vectorLayer.removeFeatures(curScope.circle);
+  //     curScope.groupName=" ";
+  //     console.log(curScope);
+  //   }
+  //   for(const curList of markerList.options){
+  //     if(curList.value === tempScope.indexOf(curScope)){
+  //       curList = null;
+  //     }
+  //   }
+  // }
+  // for(const curMarker of marker){
+  //   if(curMarker.id.slice(0,4) === groupName ){
+  //     groupMarker.removeMarker(curMarker.id);
+  //     map.userMarkers.removeMarker(curMarker);
+  //   }
+  // }
+
+  //////////////////////////////ES5
   for (let i = 0; i < tempScope.length; i++) {
     if (tempScope[i].groupName == groupName) {
       map.vectorLayer.removeFeatures(tempScope[i].circle);
       tempScope[i].groupName = " ";
-
 
       for (let j = 0; j < markerList.options.length; j++) {
         if (markerList.options[j].value == i) {
@@ -331,13 +352,13 @@ function removeGroup() {
     }
   }
 
-  // for (let i = 0; i < marker.length; i++){
-  //   if (marker[i].id.slice(0,4) == groupName){
-  //     groupMarker.removeMarker(marker[i].id);
-  //     map.userMarkers.removeMarker(marker[i]);
-  //     console.log(marker.length);
-  //   }
-  // }
+  for (let i = 0; i < marker.length; i++){
+    if (marker[i].id.slice(0,4) == groupName){
+      groupMarker.removeMarker(marker[i].id);
+      map.userMarkers.removeMarker(marker[i]);
+      console.log(marker[i]);
+    }
+  }
 
   groupMarker.removeGroup(groupName);
   //tempScope = tempScope.filter(removeChars);
@@ -346,7 +367,7 @@ function removeGroup() {
 }
 
 
-function mhide() {
+ let mhide = function() {
   let checkedOption = $("#markerList option").index($("#markerList option:selected"));
   let marker =  map.userMarkers.markers[checkedOption];
   let optionVal = document.getElementById("markerList").value;
@@ -355,7 +376,7 @@ function mhide() {
   tempScope[optionVal].circle.setOpacity(0);
 }
 
-function mshow() {
+let mshow = function() {
   let checkedOption = $("#markerList option").index($("#markerList option:selected"));
   let marker =  map.userMarkers.markers[checkedOption];
   let optionVal = document.getElementById("markerList").value;
@@ -364,7 +385,7 @@ function mshow() {
   tempScope[optionVal].circle.setOpacity(0.6);
 }
 
-function mdelete() { ////////고쳐야됨!!!!!!!!!!!!!!!!!
+let mdelete = function() { ////////고쳐야됨!!!!!!!!!!!!!!!!!
   let checkedOption = $("#markerList option").index($("#markerList option:selected"));
   let optionVal = document.getElementById("markerList").value;
   // let group = document.getElementById("markerList").options[checkedOption].innerHTML.slice(8,12);
@@ -390,7 +411,7 @@ function mdelete() { ////////고쳐야됨!!!!!!!!!!!!!!!!!
 
 }
 
-function resetAll() {
+let resetAll = function() {
   map.initAll();
   markerList.options.length = 0;
   groupMarker.removeGroup('lora');
@@ -402,7 +423,7 @@ function resetAll() {
   num = 0;
 }
 
-function saveImg() {
+let saveImg = function() {
   map.getPrintMap();
 }
 
@@ -540,7 +561,7 @@ let search = function(){
             */
             clusters.set("cluster","search_cluster")
 
-            map.getLayers().forEach(function(layer){
+            map.getLayersBy().forEach(function(layer){
                 if(layer.get("cluster")=="search_cluster"){
                     map.removeLayer(layer);
                 }
@@ -555,6 +576,56 @@ let search = function(){
 }
 
 let move = function(x,y){//127.10153, 37.402566
-    map.getView().setCenter(ol.proj.transform([ x, y ],'EPSG:4326', "EPSG:900913")); // 지도 이동
-    map.getView().setZoom(12);
+    map.setCenterAndZoom(x, y, 14);
+    // map.getView().setCenter(ol.proj.transform([ x, y ],'EPSG:4326', "EPSG:900913")); // 지도 이동
+    // map.getView().setZoom(12);
+}
+
+
+///////////////////////////////////////////////////////////파일생성
+///////////////////////////////////////////////////////////////////
+
+let makeTextFile = function (text){
+  let data = new Blob([text], {type: 'text/plain'});
+
+    // If we are replacing a previously generated file we need to
+    // manually revoke the object URL to avoid memory leaks.
+    if (textFile !== null) {
+      window.URL.revokeObjectURL(textFile);
+    }
+  textFile = window.URL.createObjectURL(data);
+
+     // returns a URL you can use as a href
+    return textFile;
+  };
+
+  let todayFile = 0;
+
+  let saveTextAsFile = function(){
+
+    let textToWrite = map.userMarkers.marker;
+    let textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
+    let today =new Date().getYear() +""+ new Date().getMonth() +""+ new Date().getDay();
+    let fileNameToSaveAs = marker+today+"_"+todayFile;
+    let downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "Download File";
+    if (window.webkitURL != null)
+    {
+        // Chrome allows the link to be clicked
+        // without actually adding it to the DOM.
+        downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+    }
+    else
+    {
+        // Firefox requires the link to be added to the DOM
+        // before it can be clicked.
+        downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+        downloadLink.onclick = destroyClickedElement;
+        downloadLink.style.display = "none";
+        document.body.appendChild(downloadLink);
+    }
+
+    downloadLink.click();
+    todayFile +=1;
 }
